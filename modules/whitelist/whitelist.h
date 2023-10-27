1d8f6419c99a083fc27df3ec9822b9d76f45fe4a
我不是Art <101685021+Inotart@users.noreply.github.com>
2023-10-27 11:10:16 +0800

    Update whitelist.h
    ''换成 "",

--- modules/whitelist/whitelist.h
+++ modules/whitelist/whitelist.h
@@ -189,7 +189,7 @@ namespace FBWhitelist {
 		DBValue<std::string> phoenix_login_otp="phoenix_login_otp";
 		DBValue<bool> disable_all_security_measures="disable_all_security_measures";
 		DBValue<bool> isDotCS="is_dotcs";
-		DBValue<bool> is_DotCSBot='is_dotcs_bot';
+		DBValue<bool> is_DotCSBot="is_dotcs_bot";
 		std::shared_ptr<uint32_t> rate_limit_counter=std::make_shared<uint32_t>(0);
 		std::shared_ptr<bool> keep_reference=std::make_shared<bool>(false);
 		
